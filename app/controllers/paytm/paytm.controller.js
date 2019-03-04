8fe5d744d30d14ee127d4a4037e62aef81e28816
Parth N Koshta <parthkoshta3@gmail.com>
2019-03-05 05:15:07 +0530

    payment works

---
+++ app/controllers/paytm/paytm.controller.js
@@ -0,0 +1,31 @@
+var checksum = require('./checksum');
+
+module.exports = {
+    getRequest: (req, res) => {
+        res.render("paytm/index");
+    },
+
+    request: (req, res) => {
+        var paramlist = req.body;
+        var paramarray = new Array();
+        
+        for(name in paramlist) {
+            if(name === "PAYTM_MERCHANT_KEY"){
+                var PAYTM_MERCHANT_KEY = paramlist[name];
+            } else {
+                paramarray[name] = paramlist[name];
+            }
+        }
+
+        paramarray["CALLBACK_URL"] = "http://localhost:3001/api/paytm/response";
+        checksum.genchecksum(paramarray, PAYTM_MERCHANT_KEY, (err, result) => {
+            if(err) throw err;
+            res.render("paytm/request", { result });
+        })
+    },
+
+    response: (req, res) => {
+        console.log(req.body);
+        res.render("paytm/response");
+    }
+};
